321feaa0247462f0c563ee02808327c17d98760d
Rrrovik <46111371+Rrrovik@users.noreply.github.com>
2021-05-04 18:10:53 +0200

    setup backend

--- src/index.js
+++ src/index.js
@@ -5,29 +5,52 @@ import cors from 'cors'
 const app = express()  // instanciranje aplikacije
 const port = 3000  // port na kojem će web server slušati
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.get('/posts', (req, res) => {
-    let posts = storage.posts
-    let query = req.query
-    
-    if (query.title) {
-        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
-    }
-    
-    if (query.createdBy) {
-        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
-    }
-    
-    if (query._any) {
-        let terms = query._any.split(" ")
-        posts = posts.filter(doc => {
-            let info = doc.title + " " + doc.createdBy
-            return terms.every(term => info.indexOf(term) >= 0)
-        })
-    }
+// za odabir vrste korisnika
+app.post('/', (req, res) => {
+    res.json({});
+})
 
-    res.json(posts)
+// sajlemo u backend odabranu ustanovu
+app.post('/select-organization', (req, res) => {
+    res.json({});
+})
+
+// sajlemo u backend odabrani redni broj korisnika
+app.post('/get-in-line', (req, res) => {
+    res.json({});
+})
+
+// prikazujemo broj trenutnog korisnika u redu
+app.get('/in-line', (req, res) => {
+    res.json({});
+})
+
+// izbacujemo korisnika iz reda jer je gotov
+app.post('/in-line', (req, res) => {
+    res.json({});
+})
+
+// spremamo na backend promijene informacije ustanove
+app.post('/change-organization-info', (req, res) => {
+    res.json({});
+})
+
+// prikazujemo dal je salter otvoren ili zatvoren
+app.get('/manage-window', (req, res) => {
+    res.json({});
+})
+
+// uzima tko je sljedeci na redu, prikazuje stanje cijelog reda
+app.get('/manage-queue', (req, res) => {
+    res.json({});
+})
+
+// mjenja stanje reda u backendu
+app.post('/manage-queue', (req, res) => {
+    res.json({});
 })
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
