324e1f1ae0c2f2c060bc5a4ba34107cbc2cd5354
Rrrovik <46111371+Rrrovik@users.noreply.github.com>
2021-09-14 17:15:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import storage from './memory_storage.js'
 import cors from 'cors'
+import dbConnection from './db.js' 
+import { restart } from 'nodemon';
+import db from './db.js';
 
 const app = express()  // instanciranje aplikacije
 const port = 3000  // port na kojem će web server slušati
@@ -8,9 +11,170 @@ const port = 3000  // port na kojem će web server slušati
 app.use(cors());
 app.use(express.json());
 
-// za odabir vrste korisnika
-app.post('/', (req, res) => {
-    res.json({});
+const lastInQueue = []
+const queue = []
+
+//registracija admina
+app.post('/registration', async (req, res) => {
+    let data = req.body
+    delete data._id
+
+    let db = await dbConnection()
+    let result = await db.collection("admins").insertOne(data)
+
+    if (result && result.acknowledged == true) {
+        res.json({
+            success: "true",
+            error: ""
+        })
+    } else {
+        res.json({
+            success: "false",
+            error: "Insert into mongoDB failed!"
+        })
+    }
+})
+
+//login admina
+app.post('/login', async (req, res) => {
+
+    let data = req.body
+    let user = null
+
+    if (data.email && data.email.trim() != "" 
+        && data. password && data.password.trim() != "") {
+        let db = await dbConnection()
+
+        user = await db.collection("admins")
+                    .findOne({
+                        email: data.email,
+                        password: data.password
+                    })
+    }
+
+    res.json(user)
+})
+
+// stvaranje nove institucije
+app.post('/institutions/create', async (req, res) => {
+    let data = req.body
+    delete data._id
+
+    let db = await dbConnection()
+    let result = await db.collection("institutions").insertOne(data)
+
+    if (result && result.acknowledged == true) {
+        res.json({
+            success: "true",
+            error: ""
+        })
+    } else {
+        res.json({
+            success: "false",
+            error: "Insert into mongoDB failed!"
+        })
+    }
+})
+
+//dohvacanje institucije
+app.get('/institutions/get', async (req, res) => {
+
+    let data = req.query
+    let institutions = null
+
+    let db = await dbConnection()
+    if (data.institution_name && data.institution_name.trim() != "") {
+        institutions = await db.collection("institutions")
+                    .findOne({
+                        institution_name: data.institution_name
+                    })
+    } else {
+        institutions = await db.collection("institutions").find().toArray()
+    }
+
+    res.json(institutions)
+})
+
+app.post('/institutions/update', async (req, res) => {
+
+    let data = req.body
+    console.log(data)
+    let db = await dbConnection()
+    await db.collection("institutions")
+      .updateOne(
+          { institution_name: data.institution_old_name },
+          {$set: {
+            institution_name: data.institution_name,
+            institution_adress: data.institution_adress,
+            institution_wh: data.institution_wh,
+            branch_office_city: data.branch_office_city
+          }},
+          {upsert: false}
+      )
+
+    res.json()
+})
+
+//dodavanje usera u red
+app.post('/queue/add', async (req, res) => {
+    let data = req.body
+
+    let lastNum = lastInQueue.find(i => i.institution_name == data.institution_name)
+    if (!lastNum) {
+        lastNum = {
+            institution_name: data.institution_name,
+            index: 1
+        }
+
+        lastInQueue.push(lastNum)
+    }
+
+    let queueUp = {
+        institution_name: data.institution_name,
+        queue_number: lastNum.index
+    }
+
+    queue.push(queueUp)
+
+    let lastNumIndex = lastInQueue.indexOf(lastNum)
+    lastInQueue[lastNumIndex].index += 1
+    
+    res.json(queueUp.queue_number)
+})
+
+//dohvacanje stanja reda za ustanovu
+app.get('/queue/size', async (req, res) => {
+    let data = req.query
+    let num = queue.filter(q => q.institution_name == data.institution_name).length
+    res.json(num)
+})
+
+//dohvacanje trenutno servirane osobe
+app.get('/queue/current', async (req, res) => {
+    let data = req.query
+    let num = queue.find(q => q.institution_name == data.institution_name).queue_number
+    res.json(num)
+})
+
+app.get('/queue/state', async (req, res) => {
+    let data = req.query
+
+    let current = queue.find(q => q.institution_name == data.institution_name)
+
+    let index = queue.indexOf(current)
+    queue.splice(index, 1)
+
+    let next = queue.find(q => q.institution_name == data.institution_name)
+
+    let size = queue.filter(q => q.institution_name == data.institution_name).length
+
+    let queueState = {
+        size,
+        current: current != undefined ? current.queue_number : 0,
+        next: next != undefined ? next.queue_number : 0
+    }
+
+    res.json(queueState)
 })
 
 // sajlemo u backend odabranu ustanovu
@@ -25,7 +189,7 @@ app.post('/get-in-line', (req, res) => {
 
 // prikazujemo broj trenutnog korisnika u redu
 app.get('/in-line', (req, res) => {
-    res.json({});
+    res.json("Hello from the server");
 })
 
 // izbacujemo korisnika iz reda jer je gotov
